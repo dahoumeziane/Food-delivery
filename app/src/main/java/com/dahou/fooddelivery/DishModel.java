09f70ecbe48a6ddd23712ce2f02d29627e5f6c30
Dahou meziane <hd_meziane@esi.dz>
2021-08-14 10:44:10 +0100

    model added

---
+++ app/src/main/java/com/dahou/fooddelivery/DishModel.java
@@ -0,0 +1,35 @@
+package com.dahou.fooddelivery;
+
+public class DishModel {
+    private String name,category,address;
+    private int image;
+    private boolean state;
+
+    public DishModel(String name, String category, String address, int image, boolean state) {
+        this.name = name;
+        this.category = category;
+        this.address = address;
+        this.image = image;
+        this.state = state;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+}
